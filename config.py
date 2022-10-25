407494839738bfde15595c87514831fd8fd5910f
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-25 17:29:50 -0500

    Se añadio un archivo con la configuración para el entorno de desarrollo y la configuración para la conexion a la base de datos

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    DEBUG=True
+    TESTING=True
+    #Configuracion de la base de datos
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:jos84mar19@localhost:3306/imagenes"
+
+class ProductionConfig(Config):
+    DEBUG=False
+
+class DevelopmentConfig(Config):
+    UPLOAD_FOLDER=os.path.realpath('.')+ '/aplication/templates/static/images/'
+    SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+    DEBUG=True
+    TESTING=True
