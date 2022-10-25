d546a6d04b033a3d92703cdf46094e3d095fb160
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-25 17:34:12 -0500

    agregue la configuración para la base de datos en el archivo interior

--- aplication/__init__.py
+++ aplication/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return "Hola"
+
+db.create_all()
