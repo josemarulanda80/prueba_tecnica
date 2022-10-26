f4854488651100cd73a42c650f4fea020b9301e8
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-26 12:52:35 -0500

    cree el modelo de la base de datos a usar en el proyecto

---
+++ aplication/models/database.py
@@ -0,0 +1,32 @@
+from aplication import db
+from datetime import datetime
+
+class Image(db.Model):
+    __tablename__="images"
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    filename=db.Column(db.String(50))
+    url=db.Column(db.String(50))
+    created=db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
+    preserts=db.relationship('Presert',backref="images",cascade="delete,merge")
+
+    def __init__(self,filename,url):
+        self.filename=filename
+        self.url=url
+    
+    def __repr__(self) -> str:
+        return f'filename : {self.filename}'
+class Presert(db.Model):
+    __tablename__="preserts"
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    img=db.Column(db.Integer,db.ForeignKey('images.id',ondelete='CASCADE'))
+    value=db.Column(db.Integer)
+    created=db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
+    
+    def __init__(self, img,value):
+        self.img=img
+        self.value=value
+
+    def __repr__(self) -> str:
+        return f'Value: {self.value}'
+        
+
