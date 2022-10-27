170504dc4db20ddcdae7f04c66ebe96d598e0ae1
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-26 19:44:40 -0500

    se creo dos funciones para llenar la base de datos con los datos solicitudes apenas se inicia la aplicación, se guarda información de las imagenes, y de los presets que deben guardarse por defecto

--- aplication/views/start.py
+++ aplication/views/start.py
@@ -1,25 +1,9 @@
+
 from aplication import db
-from flask import render_template,abort,send_from_directory,Blueprint
-from fixtures.utils import get_register
+from flask import render_template,abort,send_from_directory,Blueprint, url_for,redirect
+from fixtures.utils import get_register,insert_images,insert_images,insert_preserts
 from aplication import app
 from aplication.models.database import Image,Presert
-
-
-def add_images_data_base():
-    images=["1.jpg","2.jpg","3.jpg","4.jpg"]
-    for i in range(0,4):
-        new_image=Image(filename=images[i])
-        db.session.add(new_image)
-        db.session.commit()
-    return "ed"
-
-def add_preserts_images():
-    preserts=[0.1,0.4,0.7,0.9]
-    for i in preserts:
-            new_preserts= Presert(img=1,value=i)
-            db.session.add(new_preserts)
-            db.session.commit()
-    return "bien"
 
 init = Blueprint('/',__name__)
 
@@ -28,16 +12,26 @@ init = Blueprint('/',__name__)
 def index():
     total_images=Image.query.all()
     if total_images == []: 
-        add_images_data_base()
-        add_preserts_images()
+       
+            insert_images()
+            # 
+            return render_template("index.html")
+          
+      
     else:
-        print("Hecho")
-    return render_template("index.html")
+        insert_preserts()
+        return render_template("index.html")
+    
 
-@init.route("/probar")
-def probar():
-    print(get_register())
-    return "nada"
+@init.route("/node")
+def node():
+    preserts = Presert.query.all()
+    if preserts ==[]:
+        add_preserts_images()
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
+
 
 @init.route('/img',methods=["GET"])
 def get_file():
--- fixtures/utils.py
+++ fixtures/utils.py
@@ -1,6 +1,8 @@
 
 from asyncio import sleep
 from pymodbus.client.tcp import ModbusTcpClient
+import pymysql
+import time
 
 
 def get_register():
@@ -22,5 +24,45 @@ def get_register():
     
 
 
+def get_conexion():
+    
+    return pymysql.connect(host='localhost',
+                                user='root',
+                                password='jos84mar19',
+                                db='imagenes')
+
+def insert_images():
+    conexion = get_conexion()
+    images=["1.jpg","2.jpg","3.jpg","4.jpg"]
+    fecha = time.strftime("%y/%m/%d")
+    with conexion.cursor() as cursor:
+
+        for i in images:
+            cursor.execute("INSERT INTO images(filename,created) VALUES (%s,%s)",
+                       (i,fecha))
+    conexion.commit()
+    conexion.close()
+    conexion = get_conexion()
+    preserts=[0.1,0.4,0.7,0.9]
+    fecha = time.strftime("%y/%m/%d")
+    with conexion.cursor() as cursor:
+        for j in range(1,5):
+            for i in preserts:
+                cursor.execute("INSERT INTO preserts(img,value,created) VALUES (%s,%s,%s)",
+                        (j,i,fecha))
+    conexion.commit()
+    conexion.close()
+    insert_preserts()
 
 
+def insert_preserts():
+    conexion = get_conexion()
+    preserts=[0.1,0.4,0.7,0.9]
+    fecha = time.strftime("%y/%m/%d")
+    with conexion.cursor() as cursor:
+        for j in range(1,5):
+            for i in preserts:
+                cursor.execute("INSERT INTO preserts(img,value,created) VALUES (%s,%s,%s)",
+                        (j,i,fecha))
+    conexion.commit()
+    conexion.close()
