2c48c1acbdef785acb4f07e425a31f7e4894a55e
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-29 21:12:03 -0500

    Se arreglo el erro del cache

--- aplication/__init__.py
+++ aplication/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_caching import Cache
 
 
 app=Flask(__name__)
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
--- aplication/views/start.py
+++ aplication/views/start.py
@@ -1,10 +1,9 @@
-
 from flask import render_template, request,send_from_directory,Blueprint, url_for,redirect,request
 from sqlalchemy import values
 from fixtures.utils import get_register,insert_preserts,get_presert,created_binarization
 from aplication import app,db
 from aplication.models.database import Presert
-from aplication import cache
+
 from flask import render_template
 
 
@@ -18,7 +17,6 @@ def handle_404(e):
 """application startup endpoint that renders the main application components"""
 @bp.route("/")
 def index():
-    cache.clear()
     binarization=None
     total_images=Presert.query.all()
     if len(total_images) == 0: 
@@ -26,30 +24,28 @@ def index():
             
             return url_for('index')
     else:
-        return render_template("index.html",imagine=get_file(),preserts=total_images,binarization=binarization,message=None)
+     
+        return render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=binarization,message=None,imagen=get_register())
 
 """endpoint to display the image"""
-@bp.route('/img',methods=["GET"])
-def get_file():
-    cache.clear()
+@bp.route('/img/<int:name>',methods=["GET"])
+def get_file(name):
     """get number of modbu"""
-    name_file=get_register()
-    if name_file != "Error: Not conexion":
-        if name_file >0 and name_file <5:
-            return send_from_directory(app.config.get('UPLOAD_FOLDER'),path=f'{str(name_file)}.jpg')
+    g=name
+    if g != "Error: Not conexion":
+        if g >0 and g <5:
+            return send_from_directory(app.config.get('UPLOAD_FOLDER'),path=f'{str(g)}.jpg')
         else: 
             return None
     return None
 
 @bp.route('/image/binarization')
 def image_binarization():
-    cache.clear()
     return send_from_directory(app.config.get('UPLOAD_FOLDER'),path=f'image_binarization.jpg')
 
 """Function to be able to validate the form for presert"""
 @bp.route('/prebinarization',methods=["POST"])
 def prebinarization():
-    cache.clear()
     message=None
     if request.method == "POST":
         """check that a presert is selected"""
@@ -57,7 +53,7 @@ def prebinarization():
             presert_selected= get_presert(request.form.getlist('mycheckbox')[0])
             created_binarization(presert_selected.value,get_register())
             total_preserts=Presert.query.all()
-            return render_template("index.html",imagine=get_file(),preserts=total_preserts,binarization=True, message=None, presert=presert_selected)
+            return render_template("index.html",imagine=get_file(get_register()),preserts=total_preserts,binarization=True, message=None, presert=presert_selected, imagen=get_register())
         else:
             """Error presert selected"""
             total_images=Presert.query.all()
@@ -65,11 +61,10 @@ def prebinarization():
                 message = "No ha seleccionado alguna opción"
             if len(request.form.getlist('mycheckbox')) >0:
                 message ="Solo puede selecionar una opción"
-            return render_template("index.html",imagine=get_file(),preserts=total_images,binarization=None, message=message)
+            return render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=None, message=message,imagen=get_register())
 
 @bp.route('/presets',methods=["POST"])
 def presets():
-    cache.clear()
     if request.method == "POST":
 
         update_presert=get_presert(request.form['id'])
@@ -81,11 +76,11 @@ def presets():
                     update_presert.filename=request.form['name']
                     update_presert.value=request.form['value']
                     db.session.commit()
-                    return render_template("index.html",imagine=get_file(),preserts=total_images,binarization=None,message=None)
+                    return render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=None,message=None,imagen=get_register())
                 else:
-                     return  render_template("index.html",imagine=get_file(),preserts=total_images,binarization=None,message="No se puede editar por que el value ya esta en la base datos")
+                     return  render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=None,message="No se puede editar por que el value ya esta en la base datos",imagen=get_register())
             else:
-                 return render_template("index.html",imagine=get_file(),preserts=total_images,binarization=None,message="Error desconocido")
+                 return render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=None,message="Error desconocido",imagen=get_register())
         else:
             total_images=Presert.query.all()
             if str(update_presert.value) != str(request.form['value']):
@@ -94,14 +89,13 @@ def presets():
                 db.session.commit()
                 return redirect(url_for('.index'))
             else:
-                return  render_template("index.html",imagine=get_file(),preserts=total_images,binarization=None,message="No es posible crear, por que el valor ya existe")
+                return  render_template("index.html",imagine=get_file(get_register()),preserts=total_images,binarization=None,message="No es posible crear, por que el valor ya existe",imagen=get_register())
             
 
 
 @bp.route('/deletes/<int:id>')
 
 def delete(id):
-    cache.clear()
     delete_presert=Presert.query.filter_by(id=id).first()
     if delete_presert != None:
         db.session.delete(delete_presert)
